fb59c209013685087f3331a679c9dd659c7a845b
Sinyeong-git <ksa9408@gmail.com>
2020-01-27 21:30:11 +0900

    first upload
    first upload

---
+++ programmers/programmers/programmers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int[] answers = { 1, 2, 3, 4, 5 };
+
+        Solution p = new Solution();
+        
+        answers = p.solution(answers);
+
+    }
+    public int[] solution(int[] answers)
+    {
+        int[] answer = new int[] { };
+
+        Console.WriteLine("heelo");
+        
+  
+      
+
+        return answer;
+    }
+}
