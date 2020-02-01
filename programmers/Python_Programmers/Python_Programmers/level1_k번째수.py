3dad6592464fe4e71f66bda5c7ab5ec8c00d4d05
Sinyeong-git <ksa9408@gmail.com>
2020-02-02 02:10:14 +0900

    2020-02-02

---
+++ programmers/Python_Programmers/Python_Programmers/level1_k번째수.py
@@ -0,0 +1,13 @@
+def solution(participant, completion):
+   
+    answer = ''
+    
+
+
+
+    return answer
+
+
+input1 = ["leo", "kiki", "eden"]
+input2 = ["eden", "kiki"]
+solution(input1,input2)
