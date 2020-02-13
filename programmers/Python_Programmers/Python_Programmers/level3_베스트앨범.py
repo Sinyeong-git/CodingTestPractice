671beb55dcf61833c708c45003d7ed8f1337bb32
Sinyeong-git <ksa9408@gmail.com>
2020-02-13 19:45:01 +0900

    2020-02-13

---
+++ programmers/Python_Programmers/Python_Programmers/level3_베스트앨범.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def solution(genres, plays):
+    answer = []
+    return answer
+    Counter(genres)
+    
+
+input1 = [classic, pop, classic, classic, pop]
+input2 = [500, 600, 150, 800, 2500]
+
+print(solution(input1,input2))
