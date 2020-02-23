3d2022a1fc455f942b10278df043bf489dfb2f6b
Sinyeong-git <ksa9408@gmail.com>
2020-02-23 19:20:44 +0900

    2020-02-23
    hipq 사용법 공부중

---
+++ programmers/Python_Programmers/Python_Programmers/level2_더맵게.py
@@ -0,0 +1,12 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    return answer
+
+    heap = []
+    heapq.heappush(heap,4)
+
+input1 = [1, 2, 3, 9, 10, 12]
+input2 = 7
+print(solution(input,input2))
